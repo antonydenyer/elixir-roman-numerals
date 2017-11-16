78a7c459ce47d1788e8a5c4f60215136af5d6286
Antony Denyer <github@antonydenyer.co.uk>
2017-11-16 12:12:54 +0000

    refactor: follow the pattern for 9 and 10

--- lib/roman.ex
+++ lib/roman.ex
@@ -1,5 +1,13 @@
 defmodule Roman do
   
+  def to_roman(number, roman_accumulator) when number >= 10 do
+    to_roman(number - 10, roman_accumulator <> "X")
+  end
+
+  def to_roman(number, roman_accumulator) when number >= 9 do
+    to_roman(number - 9, roman_accumulator <> "IX")
+  end
+
   def to_roman(number, roman_accumulator) when number >= 5 do
     to_roman(number - 5, roman_accumulator <> "V")
   end
@@ -10,14 +18,6 @@ defmodule Roman do
 
   def to_roman(number, roman_accumulator) when number <= 3 do
     roman_accumulator <> String.duplicate("I", number)
-  end
-  
-  def to_roman(number) when number >= 10 do
-    to_roman(number - 10, "X")
-  end
-
-  def to_roman(number) when number >= 9 do
-    to_roman(number - 9, "IX")
   end
 
   def to_roman(number) do
