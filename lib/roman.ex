c79d40b282fc62fdcc6583a9395d0abefff7b31e
Antony Denyer <github@antonydenyer.co.uk>
2017-11-16 11:49:13 +0000

    refactor: move to a more generic implementation

--- lib/roman.ex
+++ lib/roman.ex
@@ -1,20 +1,19 @@
 defmodule Roman do
-  def to_roman(number) when number == 5 do
-    "V"
+  
   def to_roman(number, roman_accumulator) do
-    roman_accumulator <> to_roman(number)
+    roman_accumulator <> String.duplicate("I", number)
   end
   
   def to_roman(number) when number >= 5 do
     to_roman(number - 5, "V")
   end
 
-  def to_roman(number) when number == 4 do
-    "IV"
+  def to_roman(number) when number >= 4 do
+    to_roman(number - 4, "IV")
   end
 
   def to_roman(number) do
-    String.duplicate("I", number)
+    to_roman(number, "")
   end
 
 end
--- test/roman_test.exs
+++ test/roman_test.exs
@@ -1,8 +1,6 @@
 defmodule RomanTest do
   use ExUnit.Case
-  doctest Roman
 
-      
   test "one is I" do
     assert Roman.to_roman(1) == "I"
   end
